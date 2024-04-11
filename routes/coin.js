bd1f3c118a4cbbbfcb7bade5b664a6ec30894ace
Valentina Ruiz <valentinaruito@unisabana.edu.co>
2024-04-11 07:05:35 -0500

    commit1

---
+++ routes/coin.js
@@ -0,0 +1,58 @@
+const express = require("express");
+
+const router = express.Router();
+
+// /users/
+router
+  .route("/")
+  .get((req, res) => {
+    // query param
+    // req.query.[queryParamName]
+//Para que sirve
+    res.send(
+      `coin`
+    );
+  })
+  .post((req, res) => {
+    res.send(`User id: ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`User id: ${req.params.id}`);
+  });
+
+// /users/test
+router.get("/test", (req, res) => {
+  res.send("Users list test - from endpoint");
+});
+
+// /users/:id
+router.get("/:id", (req, res) => {
+  const userID = req.params.id;
+  const test = req.test;
+
+  res.send(`Listing user with ID ${userID}, ${test}`);
+});
+
+// /users/:id
+// como le queito el hi
+router.get("/:id/hi/:name", (req, res) => { //no es necesario poner : eso solo significs que es variable
+  const userID = req.params.id;
+  const userName = req.params.name;
+
+  res.send(`Listing user with ID ${userID}, say hi ${userName}`);
+});
+
+//   MIDDLEWARE
+router.param("id", (req, res, next, id) => {
+  console.log(id);
+
+  // Este código se añade al request
+  req.userId = id;
+  req.test = "asdadfsdf";
+
+  //   Next -> corre la siguiente linea -> esto es un middleware
+  // middleware -> algo que ocurre entre el momento en que una petición llega al servidor y el momento la respuesta se retorna
+  next();
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+app.use(express.urlencoded({ extended: true })); // para acceder al body
+app.use(express.json());
+
+// Routes
+const usersRouter = require("./routes/users");
+const coinRouter = require("./routes/coin");
+
+
+app.use("/coin", coinRouter);
+app.use(logger);
+
+
+app.use("/users", usersRouter);
+app.use(logger);
+
+// URL - Callback
+app.get("/", customLogger, (req, res) => {
+  res.send("Im working :)");
+});
+
+// Middleware
+function logger(req, res, next) {
+  console.log(req.originalUrl + "from logger");
+  next();
+}
+
+function customLogger(req, res, next) {
+  console.log(req.originalUrl + "from custom logger");
+  next();
+}
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+module.exports = app; 
+
