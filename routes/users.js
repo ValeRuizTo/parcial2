59caadad304d618346882325f17702c3fa3e7151
Valentina Ruiz <valentinaruito@unisabana.edu.co>
2024-04-11 15:49:28 -0500

    commit10

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,22 @@
 const express = require("express");
-
 const router = express.Router();
+const nombres = require('./nombres');
 
-// /users/
-router
-  .route("/")
-  .get((req, res) => {
-    // query param
-    // req.query.[queryParamName]
-//Para que sirve
-    res.send(
-      `users`
-    );
-  })
-  .post((req, res) => {
-    res.send(`User id: ${req.params.id}`);
-  })
-  .delete((req, res) => {
-    res.send(`User id: ${req.params.id}`);
-  });
+router.get("/:count", (req, res) => {
+  const count = parseInt(req.params.count); 
+  let sort = req.query.sort || 'ASC'; 
+  sort = sort.toUpperCase(); // Convertir sort a mayúsculas
+  
+  let sortedNombres = nombres.slice(); 
+  if (sort === 'ASC') {
+    sortedNombres.sort();
+  } else if (sort === 'DESC') {
+    sortedNombres.sort().reverse();
+  } else {
+    return res.status(400).send('El parámetro sort debe ser ASC o DESC'); 
+  }
 
+  res.send(sortedNombres.slice(0, count));
+});
 
 module.exports = router;
